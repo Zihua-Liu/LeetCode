f6f81a88f38bb5f6125ccae25f4b4d9584c6168a
Zihua Liu <liuzihua0911@gmail.com>
2018-06-05 00:09:25 +0800

    Problem 81 accepted, 0:09, June 5th, 2018

---
+++ 81/81.search-in-rotated-sorted-array-ii.python3.py
@@ -0,0 +1,68 @@
+#
+# [81] Search in Rotated Sorted Array II
+#
+# https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+#
+# algorithms
+# Medium (32.69%)
+# Total Accepted:    122.9K
+# Total Submissions: 376.8K
+# Testcase Example:  '[2,5,6,0,0,1,2]\n0'
+#
+# Suppose an array sorted in ascending order is rotated at some pivot unknown
+# to you beforehand.
+# 
+# (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+# 
+# You are given a target value to search. If found in the array return true,
+# otherwise return false.
+# 
+# Example 1:
+# 
+# 
+# Input: nums = [2,5,6,0,0,1,2], target = 0
+# Output: true
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: nums = [2,5,6,0,0,1,2], target = 3
+# Output: false
+# 
+# Follow up:
+# 
+# 
+# This is a follow up problem to Search in Rotated Sorted Array, where nums may
+# contain duplicates.
+# Would this affect the run-time complexity? How and why?
+# 
+# 
+#
+class Solution:
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: bool
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = int((left + right) / 2)
+            if nums[mid] == target:
+                return True
+            if nums[left] > nums[mid]:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            elif nums[left] < nums[mid]:
+                if target >= nums[left] and target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                left += 1
+        return False
+        
