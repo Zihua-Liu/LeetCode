d7ba8e7034b258695538bafea4793b5e412c309a
Zihua Liu <liuzihua0911@gmail.com>
2018-08-25 15:55:45 -0400

    Problem 239 accepted, 15:55, August 25th, 2018

---
+++ 239/239.sliding-window-maximum.python3.py
@@ -0,0 +1,60 @@
+#
+# [239] Sliding Window Maximum
+#
+# https://leetcode.com/problems/sliding-window-maximum/description/
+#
+# algorithms
+# Hard (34.96%)
+# Total Accepted:    103.2K
+# Total Submissions: 294.5K
+# Testcase Example:  '[1,3,-1,-3,5,3,6,7]\n3'
+#
+# Given an array nums, there is a sliding window of size k which is moving from
+# the very left of the array to the very right. You can only see the k numbers
+# in the window. Each time the sliding window moves right by one position.
+# Return the max sliding window.
+# 
+# Example:
+# 
+# 
+# Input: nums = [1,3,-1,-3,5,3,6,7], and k = 3
+# Output: [3,3,5,5,6,7] 
+# Explanation: 
+# 
+# Window position                Max
+# ---------------               -----
+# [1  3  -1] -3  5  3  6  7       3
+# ⁠1 [3  -1  -3] 5  3  6  7       3
+# ⁠1  3 [-1  -3  5] 3  6  7       5
+# ⁠1  3  -1 [-3  5  3] 6  7       5
+# ⁠1  3  -1  -3 [5  3  6] 7       6
+# ⁠1  3  -1  -3  5 [3  6  7]      7
+# 
+# 
+# Note: 
+# You may assume k is always valid, 1 ≤ k ≤ input array's size for non-empty
+# array.
+# 
+# Follow up:
+# Could you solve it in linear time?
+# 
+#
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        length = len(nums)
+        queue = []
+        ans = []
+        for i in range(len(nums)):
+        	while queue != [] and i - queue[0] >= k:
+        		queue.pop(0)
+        	while queue != [] and nums[i] >= nums[queue[-1]]:
+        		queue.pop(-1)
+        	queue.append(i)
+        	if i >= k - 1:
+        		ans.append(nums[queue[0]])
+        return ans
