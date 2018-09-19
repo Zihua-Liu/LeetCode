fad1cd5ccdb69d837ba59407e216d673bed85fa8
Zihua Liu <liuzihua0911@gmail.com>
2018-09-19 10:19:58 -0400

    Problem 275 accepted, 10:20, September 19th, 2018

---
+++ 275/275.h-index-ii.python3.py
@@ -0,0 +1,66 @@
+#
+# [275] H-Index II
+#
+# https://leetcode.com/problems/h-index-ii/description/
+#
+# algorithms
+# Medium (35.04%)
+# Total Accepted:    69.6K
+# Total Submissions: 198.8K
+# Testcase Example:  '[0,1,3,5,6]'
+#
+# Given an array of citations sorted in ascending order (each citation is a
+# non-negative integer) of a researcher, write a function to compute the
+# researcher's h-index.
+# 
+# According to the definition of h-index on Wikipedia: "A scientist has index h
+# if h of his/her N papers have at least h citations each, and the other N − h
+# papers have no more than h citations each."
+# 
+# Example:
+# 
+# 
+# Input: citations = [0,1,3,5,6]
+# Output: 3 
+# Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each
+# of them had 
+# ⁠            received 0, 1, 3, 5, 6 citations respectively. 
+# Since the researcher has 3 papers with at least 3 citations each and the
+# remaining 
+# two with no more than 3 citations each, her h-index is 3.
+# 
+# Note:
+# 
+# If there are several possible values for h, the maximum one is taken as the
+# h-index.
+# 
+# Follow up:
+# 
+# 
+# This is a follow up problem to H-Index, where citations is now guaranteed to
+# be sorted in ascending order.
+# Could you solve it in logarithmic time complexity?
+# 
+# 
+#
+class Solution:
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        if citations == []:
+        	return 0
+        left = 0
+        right = len(citations) - 1
+        while left < right:
+        	mid = (left + right) // 2
+        	if citations[mid] >= len(citations) - mid:
+        		right = mid
+        	else:
+        		left = mid + 1
+        if citations[left] >= len(citations) - left:
+        	return len(citations) - left
+        return 0
+
+        
